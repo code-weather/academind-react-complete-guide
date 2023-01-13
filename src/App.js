e916713f87852c39e6099c9fd852997aa06231c1
Jameson Wang <jamesonw10@gmail.com>
2023-01-13 17:00:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started, MY HIBBIE JIBBIE!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
